39c4581543e9fef936cc6b3e2f259b46bb4505cc
k0zmo <k0zmo@outlook.com>
2017-01-23 21:33:08 +0100

    specialize operator>> for vector<T> of trivial type T

--- src/kl/binary_rw/vector.hpp
+++ src/kl/binary_rw/vector.hpp
@@ -1,13 +1,38 @@
 #pragma once
 
 #include "kl/binary_rw.hpp"
+#include "kl/type_traits.hpp"
 
 #include <vector>
 
 namespace kl {
 
+namespace detail {
+
+// Specialized operator>> for vector<T> of trivial type T
 template <typename T>
-kl::binary_reader& operator>>(kl::binary_reader& r, std::vector<T>& vec)
+void decode_vector(kl::binary_reader& r, std::vector<T>& vec,
+                   std::true_type /*is_trivial*/)
+{
+    const auto size = r.read<std::uint32_t>();
+
+    vec.clear();
+
+    if (size)
+    {
+        auto span = r.view(size * sizeof(T));
+        if (r.err())
+            return;
+
+        vec.resize(size);
+        // Use .data() so we get a pointer and memmove fast path
+        std::copy_n(span.data(), size, vec.begin());
+    }    
+}
+
+template <typename T>
+void decode_vector(kl::binary_reader& r, std::vector<T>& vec,
+                   std::false_type /*is_trivial*/)
 {
     const auto size = r.read<std::uint32_t>();
 
@@ -23,7 +48,15 @@ kl::binary_reader& operator>>(kl::binary_reader& r, std::vector<T>& vec)
             break;
         }
     }
+}
+} // namespace detail
 
+template <typename T>
+kl::binary_reader& operator>>(kl::binary_reader& r, std::vector<T>& vec)
+{
+
+    detail::decode_vector(r, vec,
+                          kl::bool_constant<std::is_trivial<T>::value>{});
     return r;
 }
 } // namespace kl
