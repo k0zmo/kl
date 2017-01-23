4324bbdf0a88728a04d2cfe0c328a9f91557cb5b
k0zmo <k0zmo@outlook.com>
2017-01-23 21:32:10 +0100

    file_view: implementation for linux

--- src/kl/file_view.cpp
+++ src/kl/file_view.cpp
@@ -114,6 +114,101 @@ struct file_view::impl
 
 #else
 
+#include <system_error>
+#include <cassert>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+namespace kl {
+namespace detail {
+
+class file_descriptor
+{
+public:
+    file_descriptor(int fd = -1) noexcept : fd_{fd} {}
+    ~file_descriptor() { reset(); }
+
+    file_descriptor(const file_descriptor&) = delete;
+    file_descriptor& operator=(const file_descriptor&) = delete;
+
+    file_descriptor(file_descriptor&& other) noexcept
+    {
+        swap(*this, other);
+    }
+
+    file_descriptor& operator=(file_descriptor&& other) noexcept
+    {
+        assert(this != &other);
+        swap(*this, other);
+        return *this;
+    }
+
+    friend void swap(file_descriptor& l, file_descriptor& r) noexcept
+    {
+        using std::swap;
+        swap(l.fd_, r.fd_);
+    }
+
+    void reset(int fd = -1) noexcept
+    {
+        if (fd_ != -1)
+            ::close(fd_);
+        fd_ = fd;
+    }
+
+    bool operator!() const noexcept { return fd_ == -1; }
+
+    operator int() noexcept { return fd_; }
+
+private:
+    int fd_{-1};
+};
+} // namespace detail
+
+struct file_view::impl
+{
+    impl(gsl::cstring_span<> file_path)
+        : fd{::open(file_path.data(), O_RDONLY)}
+    {
+        if (!fd)
+            throw std::system_error{static_cast<int>(errno),
+                                    std::system_category()};
+
+        struct stat file_info;
+        if (fstat(fd, &file_info) == -1)
+            throw std::system_error{static_cast<int>(errno),
+                                    std::system_category()};
+
+        if (!file_info.st_size)
+            return;
+
+        file_view =
+            ::mmap(nullptr, file_info.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
+        if (!file_view)
+            throw std::system_error{static_cast<int>(errno),
+                                    std::system_category()};
+
+        contents =
+            gsl::make_span(static_cast<const byte*>(file_view),
+                           static_cast<std::ptrdiff_t>(file_info.st_size));
+    }
+
+    ~impl()
+    {
+        if (!contents.empty())
+            ::munmap(file_view, contents.size_bytes());
+    }
+
+    detail::file_descriptor fd;
+    void* file_view{nullptr};
+    gsl::span<const byte> contents;
+};
+} // namespace kl
+
 #endif
 
 namespace kl {
--- tests/file_view_test.cpp
+++ tests/file_view_test.cpp
@@ -1,9 +1,25 @@
 #include "kl/file_view.hpp"
 
 #include <catch/catch.hpp>
+#include <fstream>
+#include <string>
 
 TEST_CASE("file_view")
 {
-    kl::file_view view{"E:\\honda-dywaniki.txt"};
+    {
+        std::ofstream strm{"test.tmp",
+                           std::ios::trunc | std::ios::out | std::ios::binary};
+        strm << "Test\nHello.";
+    }
+
+    kl::file_view view{"test.tmp"};
     auto s = view.get_bytes();
+
+    REQUIRE(s.size_bytes() == 11);
+
+    std::string str;
+    str.resize(s.size_bytes());
+    std::copy_n(s.data(), s.size_bytes(), str.begin());
+
+    REQUIRE(str == "Test\nHello.");
 }
