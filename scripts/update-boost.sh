052a55270330291cf0c3c482f3292f213bc6496a
k0zmo <k0zmo@outlook.com>
2017-08-23 20:34:22 +0200

    update Boost to 1.63, CMake to 3.9.1 and clang to 3.9

--- scripts/update-boost.sh
+++ scripts/update-boost.sh
@@ -3,9 +3,9 @@
 if [ ! -d ".travis/boost/lib" ]; then
     rm -rf ".travis/boost"
     mkdir -p ".travis" && cd ".travis"
-    wget 'http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz'
-    tar xzf boost_1_60_0.tar.gz
-    cd ./boost_1_60_0
+    wget 'http://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz'
+    tar xzf boost_1_63_0.tar.gz
+    cd ./boost_1_63_0
     chmod +x bootstrap.sh
     ./bootstrap.sh --prefix="$(pwd)/../boost"
     ./b2 toolset=gcc-6 variant=release link=static "$@" install
--- scripts/update-cmake.sh
+++ scripts/update-cmake.sh
@@ -3,9 +3,9 @@
 if [ ! -d ".travis/cmake/bin" ]; then
     rm -rf ".travis/cmake"
     mkdir -p ".travis" && cd ".travis"
-    curl -O https://cmake.org/files/v3.6/cmake-3.6.1-Linux-x86_64.tar.gz
-    tar xzf cmake-3.6.1-Linux-x86_64.tar.gz
-    mv cmake-3.6.1-Linux-x86_64 cmake
+    curl -O https://cmake.org/files/v3.9/cmake-3.9.1-Linux-x86_64.tar.gz
+    tar xzf cmake-3.9.1-Linux-x86_64.tar.gz
+    mv cmake-3.9.1-Linux-x86_64 cmake
     cd cmake/bin
 else
     cd ".travis/cmake/bin"
