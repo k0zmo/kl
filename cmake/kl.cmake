2d3851440b9cbe4ded342cb563961cce2e07ac82
k0zmo <k0zmo@outlook.com>
2018-03-24 20:38:08 +0100

    reformat kl.cmake file

--- cmake/kl.cmake
+++ cmake/kl.cmake
@@ -27,34 +27,38 @@ function(target_kl_compile_options _target)
                                    # Boost still have dependencies on unary_function and binary_function, so we have to make sure not to remove them.
                                    # Also, Catch uses auto_ptr if __cplusplus is 199711L which is the MSVC case
                                    PUBLIC _HAS_AUTO_PTR_ETC=1
-                                          WIN32_LEAN_AND_MEAN)
+                                   PRIVATE WIN32_LEAN_AND_MEAN)
 
-        target_compile_options(${_target} PRIVATE /W4
-                                                  /wd4996  # Deprecated stuff
-                                                  /wd4127  # conditional expression in constant)
-                                                  /wd4100) # unreferenced formal parameter
+        target_compile_options(${_target} PRIVATE 
+                               /W4
+                               /wd4996  # Deprecated stuff
+                               /wd4127  # conditional expression in constant)
+                               /wd4100) # unreferenced formal parameter
 
     elseif(CMAKE_COMPILER_IS_GNUCXX OR (CMAKE_CXX_COMPILER_ID STREQUAL "Clang"))
-        set_target_properties(${_target}
-                              PROPERTIES CXX_STANDARD 14
-                                         CXX_STANDARD_REQUIRED ON
-                                         CXX_EXTENSIONS OFF)
+        set_target_properties(${_target} PROPERTIES
+                              CXX_STANDARD 14
+                              CXX_STANDARD_REQUIRED ON
+                              CXX_EXTENSIONS OFF)
         # TODO: CMake 3.8+
         # TODO: CMake 3.9+ MSVC also
         # target_compile_features(${_target} PUBLIC cxx_std_14)
 
-        target_compile_options(${_target} PRIVATE -Wall
-                                                  -pedantic
-                                                  -Wextra
-                                                  -Wno-unused-parameter)
+        target_compile_options(${_target} PRIVATE
+                               -Wall
+                               -pedantic
+                               -Wextra
+                               -Wno-unused-parameter)
 
         if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-            target_compile_options(${_target} PRIVATE -Wno-missing-braces)
+            target_compile_options(${_target}
+                                   PRIVATE -Wno-missing-braces)
         endif()
 
         # Warning: only if we're building all dependencies by ourselves
         if(MASTER_PROJECT AND (CMAKE_BUILD_TYPE STREQUAL "Debug"))
-            target_compile_definitions(${_target} PUBLIC _GLIBCXX_DEBUG)
+            target_compile_definitions(${_target}
+                                       PUBLIC _GLIBCXX_DEBUG)
         endif()
     endif()
 endfunction()
