e48cfd24d405078f62bb09cce7a70ee9323864a6
k0zmo <k0zmo@outlook.com>
2018-05-16 21:44:22 +0200

    Update lcov to 1.13

--- scripts/update-lcov.sh
+++ scripts/update-lcov.sh
@@ -3,9 +3,9 @@
 if [ ! -d ".travis/lcov/bin" ]; then
     rm -rf ".travis/lcov"
     mkdir -p ".travis" && cd ".travis"
-    curl -O http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz
-    tar xzf lcov_1.11.orig.tar.gz
-    mv lcov-1.11 lcov
+    curl -O http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.13.orig.tar.gz
+    tar xzf lcov_1.13.orig.tar.gz
+    mv lcov-1.13 lcov
     cd ./lcov/bin
 else
     cd ".travis/lcov/bin"
