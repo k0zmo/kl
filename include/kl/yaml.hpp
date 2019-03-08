100b885e7ae425728e2a097ecea866bc969d7627
k0zmo <k0zmo@outlook.com>
2019-03-08 22:48:17 +0100

    Implement dumping YAML directly to string

---
+++ include/kl/yaml.hpp
@@ -0,0 +1,232 @@
+#pragma once
+
+#include "kl/type_traits.hpp"
+#include "kl/ctti.hpp"
+#include "kl/enum_reflector.hpp"
+#include "kl/enum_flags.hpp"
+#include "kl/tuple.hpp"
+#include "kl/utility.hpp"
+
+#include <boost/optional.hpp>
+#include <yaml-cpp/yaml.h>
+
+#include <exception>
+#include <string>
+
+namespace kl {
+namespace yaml {
+
+template <typename T>
+struct encoder;
+
+template <typename T>
+bool is_null_value(const T& value) { return false; }
+
+template <typename T>
+bool is_null_value(const boost::optional<T>& value) { return !value; }
+
+class dump_context
+{
+public:
+    explicit dump_context(YAML::Emitter& emitter, bool skip_null_fields = true)
+        : emitter_{emitter}, skip_null_fields_{skip_null_fields}
+    {
+    }
+
+    YAML::Emitter& emitter() const { return emitter_; }
+
+    template <typename Key, typename Value>
+    bool skip_field(const Key&, const Value& value)
+    {
+        return skip_null_fields_ && is_null_value(value);
+    }
+
+private:
+    YAML::Emitter& emitter_;
+    bool skip_null_fields_;
+};
+
+template <typename T>
+std::string dump(const T& obj);
+
+template <typename T, typename Context>
+void dump(const T& obj, Context& ctx);
+
+namespace detail {
+
+KL_HAS_TYPEDEF_HELPER(value_type)
+KL_HAS_TYPEDEF_HELPER(iterator)
+KL_HAS_TYPEDEF_HELPER(mapped_type)
+KL_HAS_TYPEDEF_HELPER(key_type)
+
+KL_VALID_EXPR_HELPER(has_c_str, std::declval<T&>().c_str())
+
+template <typename T>
+struct is_map_alike
+    : conjunction<
+        has_value_type<T>,
+        has_iterator<T>,
+        has_mapped_type<T>,
+        has_key_type<T>> {};
+
+template <typename T>
+struct is_vector_alike
+    : conjunction<
+        has_value_type<T>,
+        has_iterator<T>,
+        negation<has_c_str<T>>> {};
+
+template <typename Context>
+void encode(std::nullptr_t, Context& ctx)
+{
+    ctx.emitter() << YAML::Null;
+}
+
+template <typename Map, typename Context, enable_if<is_map_alike<Map>> = true>
+void encode(const Map& map, Context& ctx)
+{
+    ctx.emitter() << YAML::BeginMap;
+    for (const auto& kv : map)
+    {
+        if (!ctx.skip_field(kv.first, kv.second))
+        {
+            ctx.emitter() << YAML::Key;
+            ctx.emitter() << kv.first;
+            ctx.emitter() << YAML::Value;
+            yaml::dump(kv.second, ctx);
+        }
+    }
+    ctx.emitter() << YAML::EndMap;
+}
+
+template <
+    typename Vector, typename Context,
+    enable_if<negation<is_map_alike<Vector>>, is_vector_alike<Vector>> = true>
+void encode(const Vector& vec, Context& ctx)
+{
+    ctx.emitter() << YAML::BeginSeq;
+    for (const auto& v : vec)
+        yaml::dump(v, ctx);
+    ctx.emitter() << YAML::EndSeq;
+}
+
+template <typename Reflectable, typename Context,
+          enable_if<is_reflectable<Reflectable>> = true>
+void encode(const Reflectable& refl, Context& ctx)
+{
+    ctx.emitter() << YAML::BeginMap;
+    ctti::reflect(refl, [&ctx](auto fi) {
+        if (!ctx.skip_field(fi.name(), fi.get()))
+        {
+            ctx.emitter() << YAML::Key;
+            ctx.emitter() << fi.name();
+            ctx.emitter() << YAML::Value;
+            yaml::dump(fi.get(), ctx);
+        }
+    });
+    ctx.emitter() << YAML::EndMap;
+}
+
+template <typename Enum, typename Context>
+void encode_enum(Enum e, Context& ctx, std::false_type /*is_enum_reflectable*/)
+{
+    yaml::dump(underlying_cast(e), ctx);
+}
+
+template <typename Enum, typename Context>
+void encode_enum(Enum e, Context& ctx, std::true_type /*is_enum_reflectable*/)
+{
+    yaml::dump(kl::to_string(e), ctx);
+}
+
+template <typename Enum, typename Context, enable_if<std::is_enum<Enum>> = true>
+void encode(const Enum& e, Context& ctx)
+{
+    encode_enum(e, ctx, is_enum_reflectable<Enum>{});
+}
+
+template <typename Enum, typename Context>
+void encode(const enum_flags<Enum>& flags, Context& ctx)
+{
+    static_assert(is_enum_reflectable<Enum>::value,
+                  "Only flags of reflectable enums are supported");
+    ctx.emitter() << YAML::BeginSeq;
+    for (const auto possible_value : enum_reflector<Enum>::values())
+    {
+        if (flags.test(possible_value))
+            yaml::dump(kl::to_string(possible_value), ctx);
+    }
+    ctx.emitter() << YAML::EndSeq;
+}
+
+template <typename Tuple, typename Context, std::size_t... Is>
+void encode_tuple(const Tuple& tuple, Context& ctx, std::index_sequence<Is...>)
+{
+    ctx.emitter() << YAML::BeginSeq;
+    using swallow = std::initializer_list<int>;
+    (void)swallow{(yaml::dump(std::get<Is>(tuple), ctx), 0)...};
+    ctx.emitter() << YAML::EndSeq;
+}
+
+template <typename... Ts, typename Context>
+void encode(const std::tuple<Ts...>& tuple, Context& ctx)
+{
+    encode_tuple(tuple, ctx, make_index_sequence<sizeof...(Ts)>{});
+}
+
+template <typename T, typename Context>
+void encode(const boost::optional<T>& opt, Context& ctx)
+{
+    if (!opt)
+        ctx.emitter() << YAML::Null;
+    else
+        return yaml::dump(*opt, ctx);
+}
+
+template <typename T, typename Context>
+void dump(const T&, Context&, priority_tag<0>)
+{
+    static_assert(always_false<T>::value,
+                  "Cannot dump an instance of type T - no viable "
+                  "definition of encode provided");
+}
+
+template <typename T, typename Context>
+auto dump(const T& obj, Context& ctx, priority_tag<1>)
+    -> decltype(ctx.emitter() << obj, void())
+{
+    ctx.emitter() << obj;
+}
+
+template <typename T, typename Context>
+auto dump(const T& obj, Context& ctx, priority_tag<2>)
+    -> decltype(encode(obj, ctx), void())
+{
+    encode(obj, ctx);
+}
+
+template <typename T, typename Context>
+auto dump(const T& obj, Context& ctx, priority_tag<3>)
+    -> decltype(yaml::encoder<T>::encode(obj, ctx), void())
+{
+    yaml::encoder<T>::encode(obj, ctx);
+}
+} // namespace detail
+
+template <typename T>
+std::string dump(const T& obj)
+{
+    YAML::Emitter emitter;
+    dump_context ctx{emitter};
+
+    yaml::dump(obj, ctx);
+    return {emitter.c_str()};
+}
+
+template <typename T, typename Context>
+void dump(const T& obj, Context& ctx)
+{
+    detail::dump(obj, ctx, priority_tag<3>{});
+}
+} // namespace yaml
+} // namespace kl
---
+++ tests/yaml_test.cpp
@@ -0,0 +1,253 @@
+#include "kl/yaml.hpp"
+#include "kl/ctti.hpp"
+#include "kl/enum_flags.hpp"
+#include "input/typedefs.hpp"
+
+#include <catch2/catch.hpp>
+#include <boost/optional/optional_io.hpp>
+
+#include <string>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <tuple>
+#include <deque>
+#include <list>
+#include <map>
+
+TEST_CASE("yaml dump")
+{
+    using namespace kl;
+
+    SECTION("basic types")
+    {
+        CHECK(yaml::dump('a') == "a");
+        CHECK(yaml::dump(1) == "1");
+        CHECK(yaml::dump(3U) == "3");
+        CHECK(yaml::dump(std::int64_t{-1}) == "-1");
+        CHECK(yaml::dump(std::uint64_t{1}) == "1");
+        CHECK(yaml::dump(true) == "true");
+        CHECK(yaml::dump(nullptr) == "~");
+        CHECK(yaml::dump("qwe") == "qwe");
+        CHECK(yaml::dump(std::string{"qwe"}) == "qwe");
+        CHECK(yaml::dump(13.11) == "13.11");
+        CHECK(yaml::dump(ordinary_enum::oe_one) == "0");
+    }
+
+    SECTION("inner_t")
+    {
+        CHECK(yaml::dump(inner_t{}) == "r: 1337\nd: 3.145926");
+    }
+
+    SECTION("different types and 'modes' for enums")
+    {
+        CHECK(yaml::dump(enums{}) ==
+              "e0: 0\ne1: 0\ne2: oe_one_ref\ne3: one");
+    }
+
+    SECTION("test unsigned types")
+    {
+        auto res = yaml::dump(unsigned_test{});
+        CHECK(
+            res ==
+            "u8: \"\\x80\"\nu16: 32768\nu32: 4294967295\nu64: 18446744073709551615");
+    }
+
+    SECTION("enum_flags")
+    {
+        auto f = make_flags(device_type::cpu) | device_type::gpu |
+                 device_type::accelerator;
+        auto res = yaml::dump(f);
+        CHECK(res == "- cpu\n- gpu\n- accelerator");
+
+        f &= ~kl::make_flags(device_type::accelerator);
+        res = yaml::dump(f);
+        CHECK(res == "- cpu\n- gpu");
+    }
+
+    SECTION("tuple")
+    {
+        auto t = std::make_tuple(13, 3.14, colour_space::lab, true);
+        auto res = yaml::dump(t);
+        CHECK(res == "- 13\n- 3.14\n- lab\n- true");
+    }
+
+    SECTION("skip serializing optional fields")
+    {
+        optional_test t;
+        t.non_opt = 23;
+
+        CHECK(yaml::dump(t) == "non_opt: 23");
+
+        t.opt = 78;
+        CHECK(yaml::dump(t) == "non_opt: 23\nopt: 78");
+    }
+
+    SECTION("don't skip optional fields if requested")
+    {
+        optional_test t;
+        t.non_opt = 23;
+
+        YAML::Emitter emitter;
+        yaml::dump_context ctx{emitter, false};
+
+        yaml::dump(t, ctx);
+        std::string res = emitter.c_str();
+        CHECK(res == "non_opt: 23\nopt: ~");
+
+        YAML::Emitter emitter2;
+        yaml::dump_context ctx2{emitter2, false};
+
+        t.opt = 78;
+        yaml::dump(t, ctx2);
+        res = emitter2.c_str();
+        CHECK(res == "non_opt: 23\nopt: 78");
+    }
+
+    SECTION("complex structure with std/boost containers")
+    {
+        auto res = yaml::dump(test_t{});
+        CHECK(
+            res ==
+            R"(hello: world
+t: true
+f: false
+i: 123
+pi: 3.1416
+a:
+  - 1
+  - 2
+  - 3
+  - 4
+ad:
+  -
+    - 1
+    - 2
+  -
+    - 3
+    - 4
+    - 5
+space: lab
+tup:
+  - 1
+  - 3.140000104904175
+  - QWE
+map:
+  1: hls
+  2: rgb
+inner:
+  r: 1337
+  d: 3.145926)");
+    }
+
+    SECTION("unsigned: check value greater than 0x7FFFFFFU")
+    {
+        REQUIRE(yaml::dump(2147483648U) == "2147483648");
+    }
+
+    SECTION("std containers")
+    {
+        auto j1 = yaml::dump(std::vector<inner_t>{inner_t{}});
+        CHECK(j1 == "- r: 1337\n  d: 3.145926");
+
+        auto j2 = yaml::dump(std::list<inner_t>{inner_t{}});
+        CHECK(j2 == "- r: 1337\n  d: 3.145926");
+
+        auto j3 = yaml::dump(std::deque<inner_t>{inner_t{}});
+        CHECK(j3 == "- r: 1337\n  d: 3.145926");
+
+        auto j4 =
+            yaml::dump(std::map<std::string, inner_t>{{"inner1", inner_t{}}});
+        CHECK(j4 == "inner1:\n  r: 1337\n  d: 3.145926");
+
+        auto j5 = yaml::dump(
+            std::unordered_map<std::string, inner_t>{{"inner2", inner_t{}}});
+        CHECK(j5 == "inner2:\n  r: 1337\n  d: 3.145926");
+    }
+}
+
+namespace {
+
+class my_dump_context
+{
+public:
+    explicit my_dump_context(YAML::Emitter& emitter)
+        : emitter_{emitter}
+    {
+    }
+
+    YAML::Emitter& emitter() const { return emitter_; }
+
+    template <typename Key, typename Value>
+    bool skip_field(const Key& key, const Value& value)
+    {
+        return !std::strcmp(key, "secret");
+    }
+
+private:
+    YAML::Emitter& emitter_;
+};
+} // namespace
+
+TEST_CASE("yaml dump - custom context")
+{
+    using namespace kl;
+
+    YAML::Emitter emitter;
+    my_dump_context ctx{emitter};
+
+    yaml::dump(struct_with_blacklisted{}, ctx);
+    std::string res = emitter.c_str();
+    CHECK(res == "value: 34\nother_non_secret: true");
+}
+
+namespace kl {
+namespace yaml {
+
+template <>
+struct encoder<std::chrono::seconds>
+{
+    template <typename Context>
+    static void encode(const std::chrono::seconds& s, Context& ctx)
+    {
+        yaml::dump(s.count(), ctx);
+    }
+};
+} // namespace yaml
+} // namespace kl
+
+TEST_CASE("yaml dump - extended")
+{
+    using namespace std::chrono;
+    chrono_test t{2, seconds{10}, {seconds{6}, seconds{12}}};
+    const auto res = kl::yaml::dump(t);
+    CHECK(res == "t: 2\nsec: 10\nsecs:\n  - 6\n  - 12");
+}
+
+template <typename Context>
+void encode(global_struct, Context& ctx)
+{
+    kl::yaml::dump("global_struct", ctx);
+}
+
+namespace my {
+
+template <typename Context>
+void encode(none_t, Context& ctx)
+{
+    kl::yaml::dump(nullptr, ctx);
+}
+
+template <typename T, typename Context>
+void encode(const value_wrapper<T>& t, Context& ctx)
+{
+    kl::yaml::dump(t.value, ctx);
+}
+} // namespace my
+
+TEST_CASE("yaml dump - overloading")
+{
+    aggregate a{{}, {}, {31}};
+    auto res = kl::yaml::dump(a);
+    CHECK(res == "g: global_struct\nn: ~\nw: 31");
+}
