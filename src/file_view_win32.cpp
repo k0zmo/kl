5e80ffcadca6ad854d6b2da8a5cea3a2af87ece2
k0zmo <k0zmo@outlook.com>
2020-03-09 18:53:25 +0100

    Use policy for defining empty state of the handle

--- src/file_view_win32.cpp
+++ src/file_view_win32.cpp
@@ -9,17 +9,17 @@ struct IUnknown; // Required for /permissive- and WinSDK 8.1
 namespace kl {
 namespace {
 
-template <LONG_PTR null>
+template <typename NullPolicy>
 class handle
 {
 public:
-    handle(HANDLE h = null_handle()) noexcept : h_{h} {}
+    handle(HANDLE h = null()) noexcept : h_{h} {}
     ~handle() { reset(); }
 
     handle(const handle&) = delete;
     handle& operator=(const handle&) = delete;
 
-    handle(handle&& other) noexcept : h_{std::exchange(other.h_, null)} {}
+    handle(handle&& other) noexcept : h_{std::exchange(other.h_, null())} {}
 
     handle& operator=(handle&& other) noexcept
     {
@@ -34,47 +34,57 @@ public:
         swap(l.h_, r.h_);
     }
 
-    void reset(HANDLE h = null_handle()) noexcept
+    void reset(HANDLE h = null()) noexcept
     {
-        if (h_ != null_handle())
+        if (h_ != null())
             ::CloseHandle(h_);
         h_ = h;
     }
 
-    bool operator!() const noexcept { return h_ == null_handle(); }
+    explicit operator bool() const noexcept { return h_ != null(); }
 
-    operator HANDLE() noexcept { return h_; }
-
-    constexpr static HANDLE null_handle() noexcept
-    {
-        return reinterpret_cast<HANDLE>(null);
-    }
+    HANDLE get() const noexcept { return h_; }
 
 private:
-    HANDLE h_{null};
+    static HANDLE null() { return reinterpret_cast<HANDLE>(NullPolicy::empty); }
+
+private:
+    HANDLE h_;
+};
+
+struct null_handle_policy
+{
+    static constexpr LONG_PTR empty{};
+};
+
+struct invalid_handle_value_policy
+{
+    // INVALID_HANDLE_VALUE cannot be assigned to HANDLE in constexpr context
+    static constexpr LONG_PTR empty = static_cast<LONG_PTR>(-1);
 };
 } // namespace
 
 file_view::file_view(const char* file_path)
 {
-    handle<(LONG_PTR)-1> file_handle{::CreateFileA(
+    handle<invalid_handle_value_policy> file_handle{::CreateFileA(
         file_path, GENERIC_READ, 0x0, nullptr, OPEN_EXISTING, 0x0, nullptr)};
     if (!file_handle)
         throw std::system_error{static_cast<int>(::GetLastError()),
                                 std::system_category()};
 
     LARGE_INTEGER file_size = {};
-    ::GetFileSizeEx(file_handle, &file_size);
+    ::GetFileSizeEx(file_handle.get(), &file_size);
     if (!file_size.QuadPart)
         return; // Empty file
 
-    handle<(LONG_PTR)0> mapping_handle{::CreateFileMappingA(
-        file_handle, nullptr, PAGE_READONLY, 0, 0, nullptr)};
+    handle<null_handle_policy> mapping_handle{::CreateFileMappingA(
+        file_handle.get(), nullptr, PAGE_READONLY, 0, 0, nullptr)};
     if (!mapping_handle)
         throw std::system_error{static_cast<int>(::GetLastError()),
                                 std::system_category()};
 
-    void* file_view = ::MapViewOfFile(mapping_handle, FILE_MAP_READ, 0, 0, 0);
+    void* file_view =
+        ::MapViewOfFile(mapping_handle.get(), FILE_MAP_READ, 0, 0, 0);
     if (!file_view)
         throw std::system_error{static_cast<int>(::GetLastError()),
                                 std::system_category()};
