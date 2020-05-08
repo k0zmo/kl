c7d5adbd137ea2f9d7d9c3573e6ef649679c1c92
k0zmo <k0zmo@outlook.com>
2020-05-08 19:01:03 +0200

    Add exception_info based on P0640

---
+++ include/kl/exception_info.hpp
@@ -0,0 +1,338 @@
+#pragma once
+
+// Based on http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2017/p0640r0.pdf
+
+#include <sstream>
+#include <memory>
+#include <type_traits>
+#include <typeinfo>
+#include <utility>
+
+#if defined(__GNUG__) && !defined(KL_NO_DEMANGLE)
+#  include <cxxabi.h>
+#  include <cstdlib>
+#endif
+
+namespace kl {
+
+namespace detail {
+
+class exception_info_node_base
+{
+public:
+    virtual ~exception_info_node_base() noexcept = default;
+
+    virtual const void* find(const std::type_info& tag) const noexcept = 0;
+    virtual void* find(const std::type_info& tag) noexcept = 0;
+    virtual void diagnostic_info(std::ostream& os) = 0;
+
+    std::shared_ptr<exception_info_node_base> next_{};
+};
+
+#if defined(__GNUG__) && !defined(KL_NO_DEMANGLE)
+
+inline auto demangled(const char* name) noexcept
+{
+    int status = -4;
+    std::unique_ptr<char, void (*)(void*)> demangled{
+        abi::__cxa_demangle(name, nullptr, nullptr, &status), std::free};
+    return demangled;
+}
+
+#  define KL_TYPE_ID_PRETTY_NAME(x) ::kl::detail::demangled((x).name()).get()
+#else
+#  define KL_TYPE_ID_PRETTY_NAME(x) (x).name()
+#endif
+
+template <typename T>
+auto diagnostic_info_impl(std::ostream& os, const std::type_info& tag,
+                          const T& value, int)
+    -> decltype((os << value), void())
+{
+    os << KL_TYPE_ID_PRETTY_NAME(tag) << " = " << value << '\n';
+}
+
+template <typename T>
+void diagnostic_info_impl(std::ostream& os, const std::type_info& tag,
+                          const T& value, ...)
+{
+    os << KL_TYPE_ID_PRETTY_NAME(tag) << " = [0x" << std::addressof(value)
+       << "]\n";
+}
+
+template <typename Tag>
+class exception_info_node : public exception_info_node_base
+{
+public:
+    explicit exception_info_node(const typename Tag::type& value)
+        : value_{value}
+    {
+    }
+    explicit exception_info_node(typename Tag::type&& value)
+        : value_{std::move(value)}
+    {
+    }
+
+    const void* find(const std::type_info& tag) const noexcept override
+    {
+        return tag == typeid(Tag) ? &value_ : nullptr;
+    }
+
+    void* find(const std::type_info& tag) noexcept override
+    {
+        return tag == typeid(Tag) ? &value_ : nullptr;
+    }
+
+    void diagnostic_info(std::ostream& os) override
+    {
+        diagnostic_info_impl(os, typeid(Tag), value_, 0);
+    }
+
+private:
+    typename Tag::type value_;
+};
+} // namespace detail
+
+class exception_info
+{
+public:
+    exception_info() noexcept = default;
+    exception_info(const char* file, int line, const char* function) noexcept
+        : file_{file}, line_{line}, function_{function}
+    {
+    }
+
+    virtual ~exception_info() noexcept = default;
+
+    exception_info(const exception_info& other) = default;
+    exception_info(exception_info&& other) noexcept = default;
+
+    exception_info& operator=(const exception_info& other) = default;
+    exception_info& operator=(exception_info&& other) noexcept = default;
+
+    [[nodiscard]] const char* file() const noexcept { return file_; }
+    [[nodiscard]] int line() const noexcept { return line_; }
+    [[nodiscard]] const char* function() const noexcept { return function_; }
+
+    template <typename Tag>
+    exception_info& set(const typename Tag::type& x)
+    {
+        auto node = std::make_shared<detail::exception_info_node<Tag>>(x);
+        node->next_ = head_;
+        head_ = node;
+        return *this;
+    }
+
+    template <typename Tag>
+    exception_info& set(typename Tag::type&& x)
+    {
+        auto node =
+            std::make_shared<detail::exception_info_node<Tag>>(std::move(x));
+        node->next_ = head_;
+        head_ = node;
+        return *this;
+    }
+
+    template <typename Tag>
+    exception_info& unset() noexcept
+    {
+        if (!head_)
+            return *this;
+
+        std::shared_ptr<detail::exception_info_node_base>* node = &head_;
+        std::shared_ptr<detail::exception_info_node_base>* prev = nullptr;
+        const auto& tag = typeid(Tag);
+
+        while (*node)
+        {
+            if ((*node)->find(tag))
+            {
+                if (prev)
+                    (*prev)->next_ = (*node)->next_;
+                else
+                    head_ = (*node)->next_;
+                break;
+            }
+
+            prev = node;
+            node = &(*node)->next_;
+        }
+
+        return *this;
+    }
+
+    template <typename Tag>
+    [[nodiscard]] const typename Tag::type* get() const noexcept
+    {
+        return const_cast<exception_info*>(this)->template get<Tag>();
+    }
+
+    template <typename Tag>
+    [[nodiscard]] typename Tag::type* get() noexcept
+    {
+        const auto& tag = typeid(Tag);
+        for (auto* node = &head_; *node;)
+        {
+            if (void* ptr = (*node)->find(tag); ptr)
+                return static_cast<typename Tag::type*>(ptr);
+            node = &(*node)->next_;
+        }
+
+        return nullptr;
+    }
+
+    [[nodiscard]] std::string diagnostic_info() const
+    {
+        std::ostringstream ss;
+        if (file_)
+            ss << file_ << '(' << line_ << ')';
+        else
+            ss << "<unknown-file>";
+
+        ss << ": throw_with_info in function "
+           << (function_ ? function_ : "<unknown-function>") << '\n';
+        for (auto* node = &head_; *node;)
+        {
+            (*node)->diagnostic_info(ss);
+            node = &(*node)->next_;
+        }
+        return ss.str();
+    }
+
+private:
+    const char* file_{};
+    int line_{};
+    const char* function_{};
+
+    // Not unique because of copyability requirement
+    std::shared_ptr<detail::exception_info_node_base> head_{};
+};
+
+namespace detail {
+
+struct exception_with_info_base : public exception_info
+{
+    exception_with_info_base(const std::type_info& exception_type,
+                             exception_info xi)
+        : exception_info{std::move(xi)}, exception_type{exception_type}
+    {
+    }
+
+    const std::type_info& exception_type;
+};
+
+template <typename E>
+class exception_with_info : public E, public exception_with_info_base
+{
+public:
+    exception_with_info(const E& e, exception_info xi)
+        : E{e}, exception_with_info_base{typeid(E), std::move(xi)}
+    {
+    }
+
+    exception_with_info(E&& e, exception_info xi)
+        : E{std::move(e)}, exception_with_info_base{typeid(E), std::move(xi)}
+    {
+    }
+};
+} // namespace detail
+
+template <typename E>
+[[noreturn]] void throw_with_info(E&& e, exception_info&& xi = exception_info())
+{
+    using e_type = std::remove_cv_t<std::remove_reference_t<E>>;
+
+    static_assert(std::is_class_v<e_type> && !std::is_final_v<e_type>);
+    static_assert(!std::is_base_of_v<exception_info, e_type>);
+
+    throw detail::exception_with_info<e_type>(std::forward<E>(e),
+                                              std::move(xi));
+}
+
+template <typename E>
+[[noreturn]] void throw_with_info(E&& e, const exception_info& xi)
+{
+    throw_with_info(std::forward<E>(e), exception_info{xi});
+}
+
+template <typename E>
+[[nodiscard]] const exception_info* get_exception_info(const E& e)
+{
+    static_assert(std::is_polymorphic_v<E>);
+    return dynamic_cast<const exception_info*>(std::addressof(e));
+}
+
+template <typename E>
+[[nodiscard]] exception_info* get_exception_info(E& e)
+{
+    static_assert(std::is_polymorphic_v<E>);
+    return dynamic_cast<exception_info*>(std::addressof(e));
+}
+
+template <typename E>
+[[nodiscard]] std::string exception_diagnostic_info(const E& e)
+{
+    std::ostringstream ss;
+
+    if constexpr (!std::is_polymorphic_v<E>)
+    {
+        ss << "dynamic exception type: " << KL_TYPE_ID_PRETTY_NAME(typeid(E))
+           << '\n';
+    }
+    else
+    {
+        const E* ep = std::addressof(e);
+        if (const auto* p =
+                dynamic_cast<const detail::exception_with_info_base*>(ep))
+        {
+            ss << "dynamic exception type: "
+               << KL_TYPE_ID_PRETTY_NAME(p->exception_type) << '\n';
+        }
+        else
+        {
+            ss << "dynamic exception type: "
+               << KL_TYPE_ID_PRETTY_NAME(typeid(e)) << '\n';
+        }
+
+        if (const auto* p = dynamic_cast<const std::exception*>(ep))
+            ss << "what: " << p->what() << '\n';
+
+        if (const auto* p = dynamic_cast<const exception_info*>(ep))
+            ss << p->diagnostic_info();
+    }
+
+    return ss.str();
+}
+
+[[nodiscard]] inline std::string
+    exception_diagnostic_info(const std::exception_ptr& p)
+try
+{
+    if (p)
+        std::rethrow_exception(p);
+    return "no exception";
+}
+catch (const exception_info& xi)
+{
+    return exception_diagnostic_info(xi);
+}
+catch (const std::exception& ex)
+{
+    return exception_diagnostic_info(ex);
+}
+catch (...)
+{
+    return "dynamic exception type: <unknown-exception>\n";
+}
+
+#if defined(_MSC_VER)
+#  define KL_FUNCTION_NAME __FUNCSIG__
+#elif defined(__GNUG__)
+#  define KL_FUNCTION_NAME __PRETTY_FUNCTION__
+#else
+#  define KL_FUNCTION_NAME __func__
+#endif
+
+#define KL_MAKE_EXCEPTION_INFO()                                               \
+    ::kl::exception_info{__FILE__, __LINE__, KL_FUNCTION_NAME}
+} // namespace kl
---
+++ tests/exception_info_test.cpp
@@ -0,0 +1,235 @@
+#include "kl/exception_info.hpp"
+#include "kl/split.hpp"
+
+#include <catch2/catch.hpp>
+
+#include <exception>
+#include <vector>
+#include <stdexcept>
+
+namespace {
+
+// clang-format off
+struct io_error: std::exception {};
+struct read_error: virtual io_error {};
+struct write_error: virtual io_error {};
+struct file_error: virtual io_error {};
+
+struct xi_file_name { using type = std::string; };
+struct xi_errno { using type = int; };
+struct xi_vec { using type = std::vector<int>;  };
+// clang-format on
+} // namespace
+
+TEST_CASE("exception_info - default constructed")
+{
+    kl::exception_info xi;
+
+    CHECK_FALSE(xi.file());
+    CHECK(xi.line() == 0);
+    CHECK_FALSE(xi.function());
+    CHECK_FALSE(xi.get<xi_errno>());
+    CHECK_FALSE(xi.get<xi_file_name>());
+
+    CHECK(xi.diagnostic_info() ==
+          "<unknown-file>: throw_with_info in function <unknown-function>\n");
+
+    kl::exception_info xi2 = xi;
+    xi = std::move(xi2);
+
+    xi.unset<xi_errno>();
+    xi.unset<xi_file_name>();
+}
+
+TEST_CASE("exception_info ")
+{
+    auto xi = KL_MAKE_EXCEPTION_INFO();
+
+    CHECK_FALSE(std::strcmp(xi.file(), __FILE__));
+    CHECK(xi.line() < __LINE__);
+    CHECK_FALSE(std::strcmp(xi.function(), KL_FUNCTION_NAME));
+    CHECK_FALSE(xi.get<xi_errno>());
+    CHECK_FALSE(xi.get<xi_file_name>());
+
+    CHECK(xi.diagnostic_info() !=
+          "<unknown-file>: throw_with_info in function <unknown-function>\n");
+
+    SECTION("stack like behaviour")
+    {
+        xi.set<xi_errno>(123);
+        REQUIRE(xi.get<xi_errno>());
+        CHECK(*xi.get<xi_errno>() == 123);
+
+        xi.set<xi_errno>(256);
+        REQUIRE(xi.get<xi_errno>());
+        CHECK(*xi.get<xi_errno>() == 256);
+
+        xi.unset<xi_errno>();
+        REQUIRE(xi.get<xi_errno>());
+        CHECK(*xi.get<xi_errno>() == 123);
+    }
+
+    SECTION("attach two types")
+    {
+        xi.set<xi_errno>(123);
+        xi.set<xi_file_name>("aaa");
+
+        REQUIRE(xi.get<xi_errno>());
+        CHECK(*xi.get<xi_errno>() == 123);
+        REQUIRE(xi.get<xi_file_name>());
+        CHECK(*xi.get<xi_file_name>() == "aaa");
+
+        SECTION("detach first errno")
+        {
+            xi.unset<xi_errno>();
+            REQUIRE_FALSE(xi.get<xi_errno>());
+            REQUIRE(xi.get<xi_file_name>());
+            CHECK(*xi.get<xi_file_name>() == "aaa");
+
+            xi.unset<xi_file_name>();
+
+            const auto& xi2 = xi;
+            REQUIRE_FALSE(xi2.get<xi_errno>());
+            REQUIRE_FALSE(xi2.get<xi_file_name>());
+        }
+
+        SECTION("detach first file_name")
+        {
+            xi.unset<xi_file_name>();
+            REQUIRE_FALSE(xi.get<xi_file_name>());
+            REQUIRE(xi.get<xi_errno>());
+            CHECK(*xi.get<xi_errno>() == 123);
+
+            xi.unset<xi_errno>();
+            REQUIRE_FALSE(xi.get<xi_errno>());
+            REQUIRE_FALSE(xi.get<xi_file_name>());
+        }
+    }
+
+    SECTION("diagnostic info")
+    {
+        auto s = xi.diagnostic_info();
+        REQUIRE(kl::split(s, "\n").size() == 1);
+
+        xi.set<xi_errno>(123);
+        s = xi.diagnostic_info();
+        REQUIRE(kl::split(s, "\n").size() == 2);
+
+        xi.set<xi_file_name>("aaa");
+        s = xi.diagnostic_info();
+        REQUIRE(kl::split(s, "\n").size() == 3);
+
+        xi.set<xi_vec>({});
+        s = xi.diagnostic_info();
+        REQUIRE(kl::split(s, "\n").size() == 4);
+    }
+}
+
+TEST_CASE("throw_with_info")
+{
+    bool thrown{};
+
+    try
+    {
+        try
+        {
+            kl::throw_with_info(
+                read_error{},
+                kl::exception_info(__FILE__, __LINE__, KL_FUNCTION_NAME)
+                    .set<xi_errno>(1234));
+        }
+        catch (kl::exception_info& xi)
+        {
+            REQUIRE(xi.get<xi_errno>());
+            xi.set<xi_file_name>("FILE_NAME.txt");
+            throw;
+        }
+    }
+    catch (const io_error& ex)
+    {
+        thrown = true;
+
+        const kl::exception_info* xi = kl::get_exception_info(ex);
+        REQUIRE(xi);
+
+        auto err = xi->get<xi_errno>();
+        REQUIRE(err);
+        CHECK(*err == 1234);
+
+        auto fn = xi->get<xi_file_name>();
+        REQUIRE(fn);
+        CHECK(*fn == "FILE_NAME.txt");
+
+        try
+        {
+            throw;
+        }
+        catch (io_error& ex)
+        {
+            kl::exception_info* xi2 = kl::get_exception_info(ex);
+            REQUIRE(xi2);
+        }
+    }
+
+    CHECK(thrown);
+}
+
+TEST_CASE("exception_diagnostic_info")
+{
+    SECTION("throw primitive")
+    {
+        try
+        {
+            throw 1;
+        }
+        catch (...)
+        {
+            auto s = kl::exception_diagnostic_info(std::current_exception());
+            REQUIRE(s == "dynamic exception type: <unknown-exception>\n");
+        }
+    }
+
+    SECTION("throw std::string")
+    {
+        try
+        {
+            throw std::string{};
+        }
+        catch (std::string& ex)
+        {
+            auto s = kl::exception_diagnostic_info(ex);
+            REQUIRE(s != "dynamic exception type: <unknown-exception>\n");
+        }
+    }
+
+    SECTION("throw runtime error")
+    {
+        try
+        {
+            throw std::runtime_error{"test error"};
+        }
+        catch (...)
+        {
+            auto s = kl::exception_diagnostic_info(std::current_exception());
+            REQUIRE_FALSE(s.empty());
+            auto ss = kl::split(s, "\n");
+            REQUIRE(ss.size() == 2);
+            REQUIRE(ss[1] == "what: test error");
+        }
+    }
+
+
+    SECTION("throw with info")
+    {
+        try
+        {
+            kl::throw_with_info(read_error(),
+                                KL_MAKE_EXCEPTION_INFO().set<xi_errno>(555));
+        }
+        catch (...)
+        {
+            auto s = kl::exception_diagnostic_info(std::current_exception());
+            REQUIRE(kl::split(s, "\n").size() == 4);
+        }
+    }
+}
