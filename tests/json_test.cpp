e0f7f1046d670e76c8263608bc111ae688e59903
k0zmo <k0zmo@outlook.com>
2020-10-04 11:49:14 +0200

    Add simple json serialization benchmarks

--- tests/json_test.cpp
+++ tests/json_test.cpp
@@ -19,7 +19,7 @@
 #include <optional>
 #include <string_view>
 
-std::string to_string(const rapidjson::Value& v)
+static std::string to_string(const rapidjson::Value& v)
 {
     rapidjson::StringBuffer buffer;
     rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
