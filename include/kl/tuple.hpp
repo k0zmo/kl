2238bbd71bff411754618d1c75f1fd23f2ff1666
k0zmo <k0zmo@outlook.com>
2020-11-11 18:11:30 +0100

    Simplify tuple-related utilities using fold expressions

--- include/kl/tuple.hpp
+++ include/kl/tuple.hpp
@@ -1,10 +1,9 @@
 #pragma once
 
+#include <iterator>
+#include <limits>
 #include <tuple>
 #include <type_traits>
-#include <limits>
-#include <algorithm>
-#include <initializer_list>
 
 namespace kl {
 
@@ -22,8 +21,7 @@ struct apply_fn
 {
 private:
     template <typename Tup, typename Fun, std::size_t... Is>
-    static auto impl(Tup&& tup, Fun&& fun, index_sequence<Is...>) -> decltype(
-        std::forward<Fun>(fun)((std::get<Is>(std::forward<Tup>(tup)))...))
+    static decltype(auto) impl(Tup&& tup, Fun&& fun, index_sequence<Is...>)
     {
         return std::forward<Fun>(fun)(
             (std::get<Is>(std::forward<Tup>(tup)))...);
@@ -31,10 +29,7 @@ private:
 
 public:
     template <typename Tup, typename Fun>
-    static auto call(Tup&& tup, Fun&& fun)
-        -> decltype(apply_fn::impl(std::forward<Tup>(tup),
-                                   std::forward<Fun>(fun),
-                                   make_tuple_indices<Tup>{}))
+    static decltype(auto) call(Tup&& tup, Fun&& fun)
     {
         return apply_fn::impl(std::forward<Tup>(tup), std::forward<Fun>(fun),
                               make_tuple_indices<Tup>{});
@@ -45,10 +40,9 @@ struct for_each_fn
 {
 private:
     template <typename Tup, typename Fun, std::size_t... Is>
-    static void impl(Tup&& tup, Fun&& fun, kl::index_sequence<Is...>)
+    static void impl(Tup&& tup, Fun&& fun, index_sequence<Is...>)
     {
-        using swallow = std::initializer_list<int>;
-        (void)swallow{(fun(std::get<Is>(std::forward<Tup>(tup))), 0)...};
+        (..., fun(std::get<Is>(std::forward<Tup>(tup))));
     }
 
 public:
@@ -56,7 +50,7 @@ public:
     static void call(Tup&& tup, Fun&& fun)
     {
         for_each_fn::impl(std::forward<Tup>(tup), std::forward<Fun>(fun),
-                          kl::make_tuple_indices<Tup>{});
+                          make_tuple_indices<Tup>{});
     }
 };
 
@@ -67,9 +61,7 @@ struct transform_ref_fn
 {
 private:
     template <typename Tup, std::size_t... Is>
-    static auto impl(Tup&& tup, index_sequence<Is...>) -> decltype(
-        std::tuple<decltype(*(std::get<Is>(std::forward<Tup>(tup))))...>{
-            (*std::get<Is>(std::forward<Tup>(tup)))...})
+    static auto impl(Tup&& tup, index_sequence<Is...>)
     {
         return std::tuple<decltype(*(std::get<Is>(std::forward<Tup>(tup))))...>{
             (*std::get<Is>(std::forward<Tup>(tup)))...};
@@ -78,8 +70,6 @@ private:
 public:
     template <typename Tup>
     static auto call(Tup&& tup)
-        -> decltype(transform_ref_fn::impl(std::forward<Tup>(tup),
-                                           make_tuple_indices<Tup>{}))
     {
         return transform_ref_fn::impl(std::forward<Tup>(tup),
                                       make_tuple_indices<Tup>{});
@@ -94,7 +84,6 @@ struct transform_deref_fn
 private:
     template <typename Tup, std::size_t... Is>
     static auto impl(Tup&& tup, index_sequence<Is...>)
-        -> decltype(std::make_tuple(*std::get<Is>(std::forward<Tup>(tup))...))
     {
         return std::make_tuple(*std::get<Is>(std::forward<Tup>(tup))...);
     }
@@ -102,8 +91,6 @@ private:
 public:
     template <typename Tup>
     static auto call(Tup&& tup)
-        -> decltype(transform_deref_fn::impl(std::forward<Tup>(tup),
-                                             make_tuple_indices<Tup>{}))
     {
         return transform_deref_fn::impl(std::forward<Tup>(tup),
                                         make_tuple_indices<Tup>{});
@@ -115,35 +102,20 @@ public:
 struct not_equal_fn
 {
 private:
-    template <typename Tup>
-    static bool impl(Tup&&, Tup&&)
-    {
-        return true;
-    }
-
-    template <std::size_t I, std::size_t... Is, typename Tup>
-    static bool impl(Tup&& tup0, Tup&& tup1)
-    {
-        return std::get<I>(std::forward<Tup>(tup0)) !=
-                   std::get<I>(std::forward<Tup>(tup1)) &&
-               not_equal_fn::template impl<Is...>(std::forward<Tup>(tup0),
-                                                  std::forward<Tup>(tup1));
-    }
-
     template <typename Tup, std::size_t... Is>
-    static bool impl2(Tup&& tup0, Tup&& tup1, index_sequence<Is...>)
+    static bool impl(Tup&& tup0, Tup&& tup1, index_sequence<Is...>)
     {
-        return not_equal_fn::impl<Is...>(std::forward<Tup>(tup0),
-                                         std::forward<Tup>(tup1));
+        return (... && (std::get<Is>(std::forward<Tup>(tup0)) !=
+                        std::get<Is>(std::forward<Tup>(tup1))));
     }
 
 public:
     template <typename Tup>
     static bool call(Tup&& tup0, Tup&& tup1)
     {
-        return not_equal_fn::impl2(std::forward<Tup>(tup0),
-                                   std::forward<Tup>(tup1),
-                                   make_tuple_indices<Tup>{});
+        return not_equal_fn::impl(std::forward<Tup>(tup0),
+                                  std::forward<Tup>(tup1),
+                                  make_tuple_indices<Tup>{});
     }
 };
 
@@ -151,61 +123,27 @@ public:
 struct distance_fn
 {
 private:
-#if defined(_MSC_VER)
-    template <typename T>
-    static const T& (min)(const T& arg)
-    {
-        return arg;
-    }
-
-    template <typename T0, typename T1, typename... Ts>
-    static const std::common_type_t<T0, T1, Ts...>&
-        (min)(const T0& arg1, const T1& arg2, const Ts&... args)
-    {
-        return (arg1 < arg2) ? (min)(arg1, args...) : (min)(arg2, args...);
-    }
-
-    // This is faster on MSVC2015 than foldl-like with an accumulator
     template <typename Tup, std::size_t... Is>
-    static std::ptrdiff_t impl2(Tup&& tup0, Tup&& tup1, index_sequence<Is...>)
+    static std::ptrdiff_t impl(Tup&& tup0, Tup&& tup1, index_sequence<Is...>)
     {
-        return (distance_fn::min)(
-            std::distance(std::get<Is>(std::forward<Tup>(tup0)),
-                          std::get<Is>(std::forward<Tup>(tup1)))...);
-    }
-#else
-    template <typename Tup>
-    static std::ptrdiff_t impl(Tup&&, Tup&&, std::ptrdiff_t acc)
-    {
-        return acc;
-    }
+        static_assert(sizeof...(Is) > 0);
 
-    template <std::size_t I, std::size_t... Is, typename Tup>
-    static std::ptrdiff_t impl(Tup&& tup0, Tup&& tup1, std::ptrdiff_t acc)
-    {
-        return distance_fn::template impl<Is...>(
-            std::forward<Tup>(tup0), std::forward<Tup>(tup1),
-            (std::min)(acc,
-                       std::distance(std::get<I>(std::forward<Tup>(tup0)),
-                                     std::get<I>(std::forward<Tup>(tup1)))));
-    }
+        std::ptrdiff_t min{std::numeric_limits<std::ptrdiff_t>::max()}, dist{};
 
-    template <typename Tup, std::size_t... Is>
-    static std::ptrdiff_t impl2(Tup&& tup0, Tup&& tup1, index_sequence<Is...>)
-    {
-        return distance_fn::impl<Is...>(
-            std::forward<Tup>(tup0), std::forward<Tup>(tup1),
-            std::numeric_limits<std::ptrdiff_t>::max());
+        return (
+            (min = (dist = std::distance(std::get<Is>(std::forward<Tup>(tup0)),
+                                         std::get<Is>(std::forward<Tup>(tup1))),
+                    dist < min ? dist : min)),
+            ...);
     }
-#endif
 
 public:
     template <typename Tup>
     static std::ptrdiff_t call(Tup&& tup0, Tup&& tup1)
     {
-        return distance_fn::impl2(std::forward<Tup>(tup0),
-                                  std::forward<Tup>(tup1),
-                                  make_tuple_indices<Tup>{});
+        return distance_fn::impl(std::forward<Tup>(tup0),
+                                 std::forward<Tup>(tup1),
+                                 make_tuple_indices<Tup>{});
     }
 };
 
@@ -213,31 +151,18 @@ public:
 struct all_true_fn
 {
 private:
-    template <typename Tup>
-    static bool impl(Tup&&)
-    {
-        return true;
-    }
-
-    template <std::size_t I, std::size_t... Is, typename Tup>
-    static bool impl(Tup&& tup)
-    {
-        return !!(std::get<I>(std::forward<Tup>(tup))) &&
-               all_true_fn::template impl<Is...>(std::forward<Tup>(tup));
-    }
-
     template <typename Tup, std::size_t... Is>
-    static bool impl2(Tup&& tup, index_sequence<Is...>)
+    static bool impl(Tup&& tup, index_sequence<Is...>)
     {
-        return all_true_fn::impl<Is...>(std::forward<Tup>(tup));
+        return (... && std::get<Is>(std::forward<Tup>(tup)));
     }
 
 public:
     template <typename Tup>
     static bool call(Tup&& tup)
     {
-        return all_true_fn::impl2(std::forward<Tup>(tup),
-                                  make_tuple_indices<Tup>{});
+        return all_true_fn::impl(std::forward<Tup>(tup),
+                                 make_tuple_indices<Tup>{});
     }
 };
 } // namespace tuple
