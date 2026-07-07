abcbe5c74b1bb610a15a9153e7d85fc8f76510a5
k0zmo <k0zmo@outlook.com>
2026-07-07 21:55:26 +0200

    Remove unused tuple functionality

--- include/kl/tuple.hpp
+++ include/kl/tuple.hpp
@@ -17,26 +17,6 @@ using make_tuple_indices =
 
 namespace tuple {
 
-// Applies a function to given tuple
-struct apply_fn
-{
-private:
-    template <typename Tup, typename Fun, std::size_t... Is>
-    static decltype(auto) impl(Tup&& tup, Fun&& fun, index_sequence<Is...>)
-    {
-        return std::forward<Fun>(fun)(
-            (std::get<Is>(std::forward<Tup>(tup)))...);
-    }
-
-public:
-    template <typename Tup, typename Fun>
-    static decltype(auto) call(Tup&& tup, Fun&& fun)
-    {
-        return apply_fn::impl(std::forward<Tup>(tup), std::forward<Fun>(fun),
-                              make_tuple_indices<Tup>{});
-    }
-};
-
 struct for_each_fn
 {
 private:
@@ -77,27 +57,6 @@ public:
     }
 };
 
-// Transforms tuple of dereferencable to tuple of dereferenced values:
-//   tuple<int*, std::optional<double>, std::vector<short>::const_iterator> ->
-//   tuple<int, double, short>
-struct transform_deref_fn
-{
-private:
-    template <typename Tup, std::size_t... Is>
-    static auto impl(Tup&& tup, index_sequence<Is...>)
-    {
-        return std::make_tuple(*std::get<Is>(std::forward<Tup>(tup))...);
-    }
-
-public:
-    template <typename Tup>
-    static auto call(Tup&& tup)
-    {
-        return transform_deref_fn::impl(std::forward<Tup>(tup),
-                                        make_tuple_indices<Tup>{});
-    }
-};
-
 // Checks for inequality with two given tuples. We AND instead of OR to
 // support zipping Sequences with different size.
 struct not_equal_fn
@@ -123,51 +82,5 @@ public:
     }
 };
 
-// Calculates minimum value of std::distance's on each pair of the tuples
-struct distance_fn
-{
-private:
-    template <typename Tup, std::size_t... Is>
-    static std::ptrdiff_t impl(Tup&& tup0, Tup&& tup1, index_sequence<Is...>)
-    {
-        static_assert(sizeof...(Is) > 0);
-
-        std::ptrdiff_t min{std::numeric_limits<std::ptrdiff_t>::max()}, dist{};
-
-        return (
-            (min = (dist = std::distance(std::get<Is>(std::forward<Tup>(tup0)),
-                                         std::get<Is>(std::forward<Tup>(tup1))),
-                    dist < min ? dist : min)),
-            ...);
-    }
-
-public:
-    template <typename Tup>
-    static std::ptrdiff_t call(Tup&& tup0, Tup&& tup1)
-    {
-        return distance_fn::impl(std::forward<Tup>(tup0),
-                                 std::forward<Tup>(tup1),
-                                 make_tuple_indices<Tup>{});
-    }
-};
-
-// Checks if all tuple elements are convertible to true
-struct all_true_fn
-{
-private:
-    template <typename Tup, std::size_t... Is>
-    static bool impl(Tup&& tup, index_sequence<Is...>)
-    {
-        return (... && std::get<Is>(std::forward<Tup>(tup)));
-    }
-
-public:
-    template <typename Tup>
-    static bool call(Tup&& tup)
-    {
-        return all_true_fn::impl(std::forward<Tup>(tup),
-                                 make_tuple_indices<Tup>{});
-    }
-};
 } // namespace tuple
 } // namespace kl
--- tests/tuple_test.cpp
+++ tests/tuple_test.cpp
@@ -9,16 +9,6 @@
 
 TEST_CASE("tuple")
 {
-    SECTION("apply_fn")
-    {
-        std::tuple<int, double> t{1, 2.0};
-        kl::tuple::apply_fn::call(t, [](int& i, double& d) {
-            i *= 2; d *= 2;
-        });
-        REQUIRE(std::get<0>(t) == 2);
-        REQUIRE(std::get<1>(t) == 4.0);
-    }
-
     SECTION("transform_ref_fn")
     {
         int arr[3]{10, 11, 12};
@@ -42,29 +32,6 @@ TEST_CASE("tuple")
         static_assert(std::tuple_size<decltype(tt)>::value == 3);
     }
 
-    SECTION("transform_deref_fn")
-    {
-        int arr[3]{10, 11, 12};
-        std::vector<short> vs = {4, 5, 6};
-        std::tuple<int*, std::optional<double>,
-                   std::vector<short>::const_iterator>
-            t{std::begin(arr), 3.0, vs.cbegin() + 1};
-
-        auto tt = kl::tuple::transform_deref_fn::call(t);
-
-        REQUIRE(std::get<0>(tt) == 10);
-        REQUIRE(std::get<1>(tt) == 3.0);
-        REQUIRE(std::get<2>(tt) == 5);
-
-        static_assert(
-            std::is_same<std::tuple_element_t<0, decltype(tt)>, int>::value);
-        static_assert(
-            std::is_same<std::tuple_element_t<1, decltype(tt)>, double>::value);
-        static_assert(
-            std::is_same<std::tuple_element_t<2, decltype(tt)>, short>::value);
-        static_assert(std::tuple_size<decltype(tt)>::value == 3);
-    }
-
     SECTION("for_each_fn")
     {
         int arr[3]{};
@@ -84,37 +51,5 @@ TEST_CASE("tuple")
         REQUIRE(kl::tuple::not_equal_fn::call(t1, t2));
         std::get<0>(t2) = 10;
         REQUIRE(!kl::tuple::not_equal_fn::call(t1, t2));
-    }
-
-    SECTION("distance_fn")
-    {
-        int arr[] = {1,2,3};
-        std::vector<int> vec = {4,5};
-
-        using tuple_type = std::tuple<int *, std::vector<int>::iterator>;
-        tuple_type t1{std::begin(arr), std::begin(vec)},
-                   t2{std::end(arr), std::end(vec)};
-
-        REQUIRE(kl::tuple::distance_fn::call(t1, t2) == 2);
-
-        vec.push_back(6);
-        t1 = tuple_type{std::begin(arr), std::begin(vec)};
-        t2 = tuple_type{std::end(arr), std::end(vec)};
-        REQUIRE(kl::tuple::distance_fn::call(t1, t2) == 3);
-
-        vec.push_back(7);
-        t1 = tuple_type{std::begin(arr), std::begin(vec)};
-        t2 = tuple_type{std::end(arr), std::end(vec)};
-        REQUIRE(kl::tuple::distance_fn::call(t1, t2) == 3);
-    }
-
-    SECTION("all_true_fn")
-    {
-        std::tuple<bool, int, std::optional<int>> t{true, 1, std::nullopt};
-        REQUIRE(!kl::tuple::all_true_fn::call(t));
-        std::get<2>(t) = 2;
-        REQUIRE(kl::tuple::all_true_fn::call(t));
-        std::get<1>(t) = 0;
-        REQUIRE(!kl::tuple::all_true_fn::call(t));
     }
 }
